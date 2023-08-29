27a9a6bf82bd38effbcbd85170b1b25c96d9ba97
miriyala srikanth <123526232+srikanthmiriyala017@users.noreply.github.com>
2023-08-29 14:52:39 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import LoginForm from "./components/LoginForm";
+import RecordingComponent from "./components/RecordingComponent";
+import ScreenRecordingComponent from "./components/ScreenRecordingComponent"; // Import the screen recording component
+import "./App.css";
+
+function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [recording, setRecording] = useState(false);
+  const [audioPermission, setAudioPermission] = useState(false);
+  const [isRecordingAudio, setIsRecordingAudio] = useState(false);
+
+  const handleLogin = (email, name) => {
+    // Simulate API call to log in user and set loggedIn state
+    setLoggedIn(true);
+  };
+
+  const handleStartRecording = () => {
+    // Start recording logic
+    setRecording(true);
+  };
+
+  const handleStopRecording = () => {
+    // Stop recording logic
+    setRecording(false);
+  };
+
+  const handleStartAudioRecording = () => {
+    // Start audio recording logic
+    setIsRecordingAudio(true);
+  };
+
+  const handleStopAudioRecording = () => {
+    // Stop audio recording logic
+    setIsRecordingAudio(false);
+  };
+
+  return (
+    <div className="app-container">
+      {!loggedIn ? (
+        <LoginForm onLogin={handleLogin} />
+      ) : (
+        <div>
+          <RecordingComponent
+            recording={recording}
+            audioPermission={audioPermission}
+            isRecordingAudio={isRecordingAudio}
+            onStartRecording={handleStartRecording}
+            onStopRecording={handleStopRecording}
+            onStartAudioRecording={handleStartAudioRecording}
+            onStopAudioRecording={handleStopAudioRecording}
+          />
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
