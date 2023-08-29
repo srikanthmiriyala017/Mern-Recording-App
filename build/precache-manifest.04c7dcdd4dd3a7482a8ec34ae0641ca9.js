8f4c610124a903297537c94ebefdf6b079fbda13
CodeSandbox <support@codesandbox.io>
2023-08-29 05:52:59 +0000

    Sandbox snapshot commit

---
+++ build/precache-manifest.04c7dcdd4dd3a7482a8ec34ae0641ca9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8973f3c1961eb7d691fb7fe4fadad64c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09b26c94c212971cc978",
+    "url": "/static/css/main.8550319a.chunk.css"
+  },
+  {
+    "revision": "f6089e0b434c3131a2ce",
+    "url": "/static/js/2.3e6600bb.chunk.js"
+  },
+  {
+    "revision": "09b26c94c212971cc978",
+    "url": "/static/js/main.8ac641d0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
---
+++ src/components/RecordingComponent.js
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+
+import "./RecordingComponent.css";
+
+const RecordingComponent = () => {
+  const [recording, setRecording] = useState(false);
+  const [mediaRecorder, setMediaRecorder] = useState(null);
+  const [recordedChunks, setRecordedChunks] = useState([]);
+  const [recordedBlobUrl, setRecordedBlobUrl] = useState(null);
+
+  const startRecording = async () => {
+    try {
+      const constraints = { video: true, audio: true };
+      const userStream = await navigator.mediaDevices.getUserMedia(constraints);
+      const recorder = new MediaRecorder(userStream);
+      const chunks = [];
+
+      recorder.ondataavailable = (event) => {
+        if (event.data.size > 0) {
+          chunks.push(event.data);
+        }
+      };
+
+      recorder.onstop = () => {
+        const blob = new Blob(chunks, { type: "video/webm" });
+        const blobUrl = URL.createObjectURL(blob);
+        setRecordedBlobUrl(blobUrl);
+        setRecordedChunks([]);
+      };
+
+      recorder.start();
+      setMediaRecorder(recorder);
+      setRecording(true);
+    } catch (error) {
+      console.error("Error starting recording:", error);
+    }
+  };
+
+  const stopRecording = () => {
+    if (mediaRecorder) {
+      mediaRecorder.stop();
+      setRecording(false);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Recording Component</h2>
+      {!recording ? (
+        <button onClick={startRecording}>Start Recording</button>
+      ) : (
+        <button onClick={stopRecording}>Stop Recording</button>
+      )}
+
+      {recordedBlobUrl && (
+        <div>
+          <video controls src={recordedBlobUrl} />
+          <a href={recordedBlobUrl} download="recorded-video.webm">
+            Save Recorded Video
+          </a>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default RecordingComponent;
